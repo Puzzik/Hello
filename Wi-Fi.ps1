e519aa0c43d2a4e5372263b2debce115f8d071ef
Max Kamenskiy <puzzik@MacBook-Pro-Max.local>
2020-10-07 14:49:57 +0300

    Second commit

--- Wi-Fi.ps1
+++
@@ -1,4 +0,0 @@
-$strSSID = "HomeNet5"
-$strWiFipassword = "MAX17111993"
-$strPath = "$home\desktop\Contact\wifi.png"
-New-QRCodeWifiAccess -SSID $strSSID -Password $strWiFipassword -Width 10 -OutPath $strPath
